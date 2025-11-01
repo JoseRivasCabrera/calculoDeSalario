24facb1a9f5624d7bdca18de91de12a75f367237
jose.trabajo877 <jose.trabajo877@gmail.com>
2025-11-01 07:01:30 +0000

    implementacion de inicio de sesion con google

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,31 +1,31 @@
 function handleCredentialResponse(response) {
-    console.log('ID token:', response.credential);
-    
-    // Enviar al backend para verificación
-    fetch('/session/login', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ id_token: response.credential })
-    })
-    .then(r => r.json())
-    .then(data => {
-      console.log('Respuesta backend:', data);
-      alert(`Bienvenido ${data.user.name} (${data.user.email})`);
-    })
-    .catch(err => console.error('Error:', err));
-  }
+  console.log('ID token:', response.credential);
   
-  window.onload = function () {
-    google.accounts.id.initialize({
-      client_id: '358858690615-vmfeph3nr3kq9kquadh92qj8uev72rk0.apps.googleusercontent.com',
-      callback: handleCredentialResponse
-    });
-  
-    google.accounts.id.renderButton(
-      document.getElementById('buttonDiv'),
-      { theme: 'outline', size: 'large' }
-    );
-  
-    // Muestra One Tap
-    google.accounts.id.prompt();
-  };
+  // Enviar al backend para verificación
+  fetch('/session/login', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ id_token: response.credential })
+  })
+  .then(r => r.json())
+  .then(data => {
+    console.log('Respuesta backend:', data);
+    alert(`Bienvenido ${data.user.name} (${data.user.email})`);
+  })
+  .catch(err => console.error('Error:', err));
+}
+
+window.onload = function () {
+  google.accounts.id.initialize({
+    client_id: '358858690615-vmfeph3nr3kquadh92qj8uev72rk0.apps.googleusercontent.com',
+    callback: handleCredentialResponse
+  });
+
+  google.accounts.id.renderButton(
+    document.getElementById('buttonDiv'),
+    { theme: 'outline', size: 'large' }
+  );
+
+  // Muestra One Tap
+  google.accounts.id.prompt();
+};
